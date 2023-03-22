a8de5d91a4480ce1bd2e60aade97db2095b04126
Pritam-debnath <pritamd@geekyants.com>
2023-03-22 14:59:50 +0530

    middleware for username and role

---
+++ src/middleware/verifySignup.js
@@ -0,0 +1,40 @@
+const db = require("../../database/models");
+const ROLES = db.ROLES;
+const User = db.user;
+
+checkDuplicateUsername = (req, res, next) => {
+  //for username
+  User.findOne({
+    where: {
+      username: req.body.username,
+    },
+  }).then((user) => {
+    if (user) {
+      res.status(400).send({ message: "Username already exists" });
+      return;
+    }
+    next();
+  });
+};
+
+checkRolesExisted = (req, res, next) => {
+  if (req.body.roles) {
+    for (let i = 0; i < req.body.roles.length; i++) {
+      if (!ROLES.includes(req.body.roles[i])) {
+        res.status(400).send({
+          message: "Failed! Role does not exist = " + req.body.roles[i],
+        });
+        return;
+      }
+    }
+  }
+
+  next();
+};
+
+const verifySignUp = {
+  checkDuplicateUsername: checkDuplicateUsername,
+  checkRolesExisted: checkRolesExisted,
+};
+
+module.exports = verifySignUp;
