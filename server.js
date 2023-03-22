fa492bd7b42682738a3aa0841108f89967fe7949
Pritam-debnath <pritamd@geekyants.com>
2023-03-22 17:41:51 +0530

    initialised the project

--- server.js
+++ server.js
@@ -1,45 +1,14 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
 const app = express();
-const db = require("./database/models");
-const Role = db.role;
 
-const PORT = 8000;
-
-function initial() {
-  Role.create({
-    id: 1,
-    name: "user",
-  });
-
-  Role.create({
-    id: 2,
-    name: "moderator",
-  });
-
-  Role.create({
-    id: 3,
-    name: "admin",
-  });
-}
-db.sequelize.sync({ force: true }).then(() => {
-  console.log("Drop and Resync database");
-  initial();
-});
-//routes
-require("./src/routes/auth")(app);
-require("./src/routes/user")(app);
-//to enable CORS
-var corsOptions = {
-  origin: "http://localhost:8081",
-};
-
-app.use(cors(corsOptions));
+const PORT = 8080;
 
 //helps to parse the request and create the req.body object
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.json({ message: "this is a node postgres JWT auth API" });
