988ec5c11014b4c42b23f96a6621b7c921772176
Pritam-debnath <pritamd@geekyants.com>
2023-03-29 23:08:38 +0530

    user restricted to alter data

--- src/services/todo.js
+++ src/services/todo.js
@@ -13,30 +13,30 @@ const getTodo = async (request, response) => {
 const createTodo = async (request, response) => {
   const { title, isCompleted } = request.body;
   const userId = request.user.uuid;
-
   const user = await User.findOne({ where: { uuid: userId } });
   if (!user) {
     return response.status(404).send({ message: "User not found" });
   }
-
   const newTodo = {
     title,
     isCompleted,
     userId,
   };
-
   await Todo.create(newTodo)
     .then((data) => response.send(data))
     .catch((err) => response.status(400).send(err));
 };
 
 const deleteTodo = async (request, response) => {
-  console.log("HERE IS DELETE!!!!");
+  const userId = request.user.uuid;
   const { uuid } = request.params;
-  console.log(`UUID TO DELETE==>${uuid}`);
-
+  //check here to see if the authenticated user deleting is their own todo
   const todo = await Todo.findOne({ where: { uuid: uuid } });
-
+  if (userId != todo.userId) {
+    return response.status(404).send({
+      message: "THIS USER IS NOT ALLOWED to delete this Todo",
+    });
+  }
   await todo
     .destroy()
     .then(() => response.send("Todo deleted!"))
@@ -46,13 +46,16 @@ const deleteTodo = async (request, response) => {
 const updateTodo = async (request, response) => {
   const { uuid } = request.params;
   const { isCompleted } = request.body;
-
+  //check here to see if the authenticated user updating is their own todo
   const todo = await Todo.findOne({ where: { uuid } });
-
+  const userId = request.user.uuid;
+  if (userId != todo.userId) {
+    return response.status(404).send({
+      message: "THIS USER IS NOT ALLOWED to make changes in this Todo",
+    });
+  }
   todo.isCompleted = isCompleted;
-
   await todo.save();
-
   response.send(todo);
 };
 
