2f1e03c41967f4ea9243cf4262989802477de0df
Pritam-debnath <pritamd@geekyants.com>
2023-03-29 23:25:09 +0530

    used .env for storing sensitive data

--- database/config/index.js
+++ database/config/index.js
@@ -1,13 +1,17 @@
+require("dotenv").config();
+
 module.exports = {
-  JWT_SECRET: "0P3NL3TTER",
-  JWT_REFRESH_SECRET: "HJE5678THUG834567THJG84576TEHJGT",
+  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET,
   DB_INFO: {
     development: {
-      username: "pritamdebnath",
-      password: "",
-      database: "todo_app",
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       host: "localhost",
       dialect: "postgres",
     },
   },
 };
+
+//.env stores these data
