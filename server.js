88845dfee6b45fabca9c3ea386c7de305b0d3a6e
Pritam-debnath <pritamd@geekyants.com>
2023-04-26 09:14:15 +0530

    Revert "session support to maintain states"
    This reverts commit 4716f35de700712f05bb4722a370cfef7988d347.

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
 const express = require("express");
-const path = require("path");
 const cookieParser = require("cookie-parser");
-const session = require("express-session");
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const db = require("./database/models");
 const router = require("./src/routes");
 const app = express();
@@ -19,21 +16,6 @@ app.set("view engine", "ejs");
 db.sequelize.sync({ force: false }).then(() => {
   console.log("DB has been resynced!");
 });
-
-app.use(express.static(path.join(__dirname, "public")));
-app.use(
-  session({
-    secret: "openletter",
-    resave: false,
-    saveUninitialized: false,
-    store: new SequelizeStore({
-      db: db.sequelize,
-      table: "sessions",
-      checkExpirationInterval: 15 * 60 * 1000, // The interval at which to cleanup expired sessions in milliseconds.
-      expiration: 24 * 60 * 60 * 1000, // The maximum age (in milliseconds) of a valid session.
-    }),
-  })
-);
 router(app);
 
 app.listen(PORT, () => {
