179f9e192055489a21137642d24e0087b7d09241
Pritam-debnath <pritamd@geekyants.com>
2023-04-26 09:14:29 +0530

    Revert "google strategy is created"
    This reverts commit 78158318301ae23927358d89055ed790566ee985.

--- database/config/index.js
+++ database/config/index.js
@@ -3,9 +3,6 @@ require("dotenv").config();
 module.exports = {
   JWT_SECRET: "0P3NL3TTER",
   JWT_REFRESH_SECRET: "HJE5678THUG834567THJG84576TEHJGT",
-  CLIENT_ID:
-    "523872834988-66fnc3ni5j6bhuff8rf88kad2f3e5spj.apps.googleusercontent.com",
-  CLIENT_SECRET: "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",
   DB_INFO: {
     development: {
       username: "pritamdebnath",
