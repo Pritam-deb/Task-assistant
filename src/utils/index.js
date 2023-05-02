a88507274a0f63947eb2db2379fe0d697735fe80
Pritam-debnath <pritamd@geekyants.com>
2023-05-02 18:05:46 +0530

    feat: consolidated and sent single email to user

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,7 +2,7 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const nodemailer = require("nodemailer");
 const cron = require("node-cron");
-const { Op } = require("sequelize");
+const { Op, Sequelize } = require("sequelize");
 const { JWT_SECRET } = require("../../database/config");
 
 const db = require("../../database/models");
@@ -47,7 +47,6 @@ const checkTodosDueInThreeDays = async () => {
   const threeDaysFromNow = new Date(
     new Date().getTime() + 3 * 24 * 60 * 60 * 1000
   );
-
   const todos = await Todo.findAll({
     where: {
       dueDate: {
@@ -60,16 +59,44 @@ const checkTodosDueInThreeDays = async () => {
         attributes: ["userEmail"],
       },
     ],
+    order: [["dueDate", "ASC"]],
   });
-
+  const todosByDueDate = {};
   if (todos) {
     todos.forEach((todo) => {
-      const subject = `Reminder: Your todo "${todo.title}" is due in 3 days`;
-      const text = `Your todo "${
-        todo.title
-      }" is due on ${todo.dueDate.toDateString()}. Don't forget to complete it!`;
-      sendEmail(todo.user.userEmail, subject, text);
+      const dueDate = todo.dueDate.toISOString().substring(0, 10);
+      if (todosByDueDate[dueDate]) {
+        todosByDueDate[dueDate].push(todo);
+      } else {
+        todosByDueDate[dueDate] = [todo];
+      }
     });
+  }
+  for (const dueDate in todosByDueDate) {
+    const todos = todosByDueDate[dueDate];
+    await sendReminderEmail(todos);
+  }
+};
+
+const sendReminderEmail = async (todos) => {
+  // group todos by user email
+  const todosByUser = {};
+  todos.forEach((todo) => {
+    const userEmail = todo.user.userEmail;
+    if (todosByUser[userEmail]) {
+      todosByUser[userEmail].push(todo.title);
+    } else {
+      todosByUser[userEmail] = [todo.title];
+    }
+  });
+
+  // send email to each user
+  for (const userEmail in todosByUser) {
+    const subject = "Reminder: You have todo/s due soon";
+    const body = todosByUser[userEmail].join("\n");
+    // console.log(`TODOS BY USER========>`, body);
+
+    await sendEmail(userEmail, subject, body);
   }
 };
 
