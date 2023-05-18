26d54e6945587d1c151dca3b8c09fc258d80be0a
Pritam-debnath <pritamd@geekyants.com>
2023-05-18 12:18:17 +0530

    feat: used lints and its working

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    node: true,
+    commonjs: true,
+    es2021: true,
+    // "jest/globals": true,
+  },
+  //   plugins: ["jest"],
+  //   extends: ["standard", "plugin:jest/recommended"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {},
+};
